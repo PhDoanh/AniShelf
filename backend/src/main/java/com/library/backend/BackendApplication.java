d7131a77b25942a1e81530d45be15bdba7b55c29
Pham Doanh <pgdoanh711@gmail.com>
2025-03-20 23:38:33 +0700

    refactor(all): Initialize the project structure

---
+++ backend/src/main/java/com/library/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+// Đây là file mẫu để tham khảo
+
+package com.library.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/library/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+// Đây là file mẫu để tham khảo
+
+package com.library.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
