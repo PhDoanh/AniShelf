b790bcf3e550934b9e72e893745e8c056c82e0b2
Pham Doanh <pgdoanh711@gmail.com>
2025-05-09 10:21:03 +0700

    feat: implement ServiceHandler for centralized service management

---
+++ src/main/java/com/library/anishelf/service/ServiceHandler.java
@@ -0,0 +1,13 @@
+package com.library.anishelf.service;
+
+/**
+ * The interface Service handler.
+ */
+public interface ServiceHandler {
+    /**
+     * Handle request boolean.
+     *
+     * @return the boolean
+     */
+    boolean handleRequest();
+}
